483beeb0f17aec279b0deadaae1922166a2ad93c
Thiago <thberenger27@gmail.com>
2025-03-21 17:20:52 -0300

    estilizando icones dos sociais

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,38 @@
+import Social from '@/components/Social'
+import { Button } from '@/components/ui/button'
+import { FiDownload } from 'react-icons/fi'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>Homepage</main>
-  );
+    <section className='h-full'>
+      <div className='container mx-auto h-full'>
+        <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+          <div className='text-center xl:text-left'>
+            <span className='text-xl'>Desenvolvedor Front-end</span>
+            <h1 className='h1 mb-6'>
+              <span className='text-accent'>Thiago Berenger</span>
+            </h1>
+            <p className='max-w-[500px] mb-9 text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui, quis excepturi modi necessitatibus maxime. Voluptas enim, minus dicta cumque aspernatur repellat corporis quod.</p>
+            <div className='flex flex-col xl:flex-row items-center gap-8'>
+            <Button 
+            variant='outline' 
+            size='lg' 
+            className='uppercase flex items-center gap-2'>
+              <span>Baixar currículo</span>
+              <FiDownload className='text-xl' />
+            </Button>
+            <div className='mb-8 xl:mb-0'>
+              <Social 
+              containerStyles='flex gap-6' 
+              iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:text-primary hover:transition-all duration-500' />
+            </div>
+          </div>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
