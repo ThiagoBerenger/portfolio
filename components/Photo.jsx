7c5cd23518bd7af763889f37662dc21a09b94753
Thiago <thberenger27@gmail.com>
2025-03-23 11:17:06 -0300

    estilizando svg sobre foto

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -12,21 +12,54 @@ const Photo = () => {
             transition: {delay: 2, duration: .5, ease: 'easeIn'}
          }}    
         >
+          {/* imagem */}
             <motion.div 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1,
                 transition: {delay: 2.4, duration: .5, ease: 'easeInOut'}
              }}    
-            className="w-[298] h-[298] xl:w-[498] xl:h-[498] mix-blend-lighten">
+            className="mix-blend-lighten">
                 <Image src='/assets/photo.png' 
                 priority quality={100} 
                 fill
                 alt=""
                 className="obect-contain rounded-full"/>
             </motion.div>
+          {/* circulo */}
+          <motion.svg 
+          className='w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]' 
+          fill='transparent'
+          viewBox='0 0 506 506'
+          xmlns='http://www.w3.org/2000/svg'
+          >
+            <motion.circle 
+            cx='253' 
+            cy='253' 
+            r='250' 
+            stroke='#00ff99' 
+            strokeWidth='4'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            initial={{ strokeDasharray: '24 10 0 0'}}
+            animate={{ 
+                strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+                rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20, 
+              repeat: Infinity, 
+              repeatType: 'reverse'}}
+            />
+          </motion.svg>
         </motion.div>
     </div>
   )
 }
 
 export default Photo
+
+
+// https://www.youtube.com/watch?v=dImgZ_AH7uA 1:10:54
+
+
+
