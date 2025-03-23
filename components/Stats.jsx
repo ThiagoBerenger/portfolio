b0e189969c087919207121a99721a29d4bba4221
Thiago <thberenger27@gmail.com>
2025-03-23 11:30:58 -0300

    criando Stats

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,8 +1,44 @@
 'use client'
 
+import CountUp from "react-countup"
+
+const stats = [
+    {
+        num: 2,
+        text: 'anos de experiência'
+    },
+    {
+        num: 2,
+        text: 'formações'
+    },
+    {
+        num: 4,
+        text: 'certificados'
+    },
+    {
+        num: 200,
+        text: 'commits no GitHub'
+    }
+]
+
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+        <div className="container mx-auto">
+            <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                {stats.map((item, index) => {
+                    return <div className="flex-1 flex gap-4 items-center justify-center x:justify-start" key={index}>
+                        <CountUp
+                        end={item.num}
+                        duration={5}
+                        delay={2}
+                        className="4xl xl:text-6xl font-extrabold"/>
+                        <p className={`${item.text.length < 15 ? 'max-w-[100px]':'max-w-[150px]'} leading-snug text-white/80 `}>{item.text}</p>
+                    </div>
+                })}
+            </div>
+        </div>
+    </section>
   )
 }
 
