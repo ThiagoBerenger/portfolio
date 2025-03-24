9e00a364971a56358d7ea003e8d0229cd0ff001d
Thiago <thberenger27@gmail.com>
2025-03-24 17:59:49 -0300

    finalizando página sobre

--- app/sobre/page.jsx
+++ app/sobre/page.jsx
@@ -20,12 +20,12 @@ const about = {
       fieldValue: '2 anos'
     },
     {
-      fieldName: 'Nacionalidade',
-      fieldValue: 'Brasileiro'
-    },
-    {
       fieldName: 'Email',
       fieldValue: 'thberenger27@gmail.com'
+    },
+    {
+      fieldName: 'Nacionalidade',
+      fieldValue: 'Brasileiro'
     },
     {
       fieldName: 'Freelance',
@@ -81,7 +81,7 @@ const skills = {
     },
     {
       icon: <FaCss3/>,
-      name: 'css 3'
+      name: 'CSS3'
     },
     {
       icon: <FaJs/>,
@@ -125,37 +125,65 @@ const Sobre = () => {
         className='flex flex-col xl:flex-row gap-[60px]'>
           <TabsList className='flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6'>
             <TabsTrigger value='about'>Sobre mim</TabsTrigger>
-            <TabsTrigger value='education'>Certificados</TabsTrigger>
             <TabsTrigger value='skills'>Habilidades</TabsTrigger>
+            <TabsTrigger value='education'>Certificados</TabsTrigger>
           </TabsList>
           <div className="min-h-[70vh] w-full">
             <TabsContent value='about'className='w-full'>
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{about.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
-                <ScrollArea className="h-[400px] overflow-auto">
-                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                    {education.items.map((item, index) => {
-                      return (
-                        <li key={index} className="bg-[#232329] h-[220px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
-                        <span className="text-accent">{item.duration}</span>
-                        <h3 className="text-lg/6 max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
-                        <div className="flex items-center gap-3">
-                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
-                          <p className="text-white/60">{item.institution}</p>
-                        </div>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[600px] mx-auto xl:mx-0">
+                  {about.info.map((item, index)=> {
+                    return <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                      <span className="text-white/60">{item.fieldName}</span>
+                      <span className="text-xl">{item.fieldValue}</span>
                       </li>
-                      )
-                    })}
-                  </ul>
-                </ScrollArea>
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+            <TabsContent value='skills'className='w-full h-full'>
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index)=> {
+                    return <li key={index}>
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                            <div className="text-6xl group-hover:text-accent transition-all diration-300">{skill.icon}</div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{skill.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  })}
+                </ul>
               </div>
             </TabsContent>
             <TabsContent value='education'className='w-full'>
-              Formações
-            </TabsContent>
-            <TabsContent value='skills'className='w-full'>
-              Habilidades
+              <ScrollArea className="h-[620px] overflow-auto">
+                    <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                      {education.items.map((item, index) => {
+                        return (
+                          <li key={index} className="bg-[#232329] h-[220px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-lg/6 max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                        )
+                      })}
+                    </ul>
+                  </ScrollArea>
             </TabsContent>
           </div>
         </Tabs>
